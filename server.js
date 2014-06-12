5b38a0ef585887952f0eea8b4f1636eadac50560
neilyolol <yckasim00@163.com>
2014-06-12 21:28:17 +0800

    initial

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
+var http = require("http");
 
+function start(){
+	function onRequest(request,response){
+		console.log("Request received.");
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write("Welcome to Component Owner Search");
+		response.end();
+}
+
+http.createServer(onRequest).listen(8090);
+
+console.log("Server has been started");
+}
+
+exports.start = start;
